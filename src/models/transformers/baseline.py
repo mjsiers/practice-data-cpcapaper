f87f94044bfba2c4cd80c6123e95452901316a12
Michael Siers <msiers@saturnsys.com>
2019-03-06 10:45:33 -0600

    Removed unused code.  Added parameter to output baseline instead of corrected signal.

--- src/models/transformers/baseline.py
+++ src/models/transformers/baseline.py
@@ -1,6 +1,4 @@
 import numpy as np
-from scipy.interpolate import UnivariateSpline
-from scipy.spatial import ConvexHull
 from sklearn.base import BaseEstimator, TransformerMixin
 
 def fitpoly(y, xvals=None, polyorder=2):     
@@ -8,41 +6,15 @@ def fitpoly(y, xvals=None, polyorder=2):
     ypoly = np.polyval(coeffs, xvals)
     return ypoly
 
-# https://dsp.stackexchange.com/questions/2725/how-to-perform-a-rubberband-correction-on-spectroscopic-data
-def fitband(y, usespline=True, usepoly=False, polyorder=5):
-    x = np.arange(0, y.shape[0], 1.0) 
-
-    # find the convex hull
-    pts = np.array(list(zip(x, y)))
-    v = ConvexHull(pts).vertices 
-
-    # rotate convex hull vertices until they start from the lowest one
-    v = np.roll(v, -v.argmin())
-    # leave only the ascending ones
-    v = v[:v.argmax()]
-
-    if usespline:
-        # fit spline to the convex hull
-        coeffs = UnivariateSpline(x[v], y[v], s=None)
-        baseline = coeffs(x)
-    elif usepoly:
-        # fit polynomial to the convex hull data only
-        polycoeffs = np.polyfit(x[v], y[v], polyorder)
-        baseline = np.polyval(polycoeffs, x)
-    else:
-        # create baseline using linear interpolation between vertices
-        baseline = np.interp(x, x[v], y[v])
-    
-    return baseline
-
 class Baseline(BaseEstimator, TransformerMixin):
     ''' Baseline correction using polynomial. '''
-    def __init__(self, polyorder=2, weight=0.95, allowneg=False, negthreshold=0.0001):
+    def __init__(self, polyorder=2, weight=0.95, allowneg=False, negthreshold=0.0001, outbaseline=False):
         ''' Called when initializing the transformer.  '''
         self.polyorder = polyorder
         self.weight = weight
         self.allowneg = allowneg
         self.negthreshold = negthreshold
+        self.outbaseline = outbaseline
     
     def fit(self, X, y=None):
         return self
@@ -58,5 +30,9 @@ class Baseline(BaseEstimator, TransformerMixin):
         if not self.allowneg:
             # set all negative values to zero
             Xdata[Xdata < self.negthreshold] = 0.0
+        
+        # check to see if we need to output the baseline and not the corrected signal
+        if self.outbaseline:
+            return Xbase
 
         return Xdata
