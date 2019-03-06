c9a5e7f698ef84dbfc05bac3cfc6406fe9d6031b
Michael Siers <msiers@saturnsys.com>
2019-03-06 09:56:57 -0600

    Updated baseline generation notebook.

--- notebooks/generators/workbooks/02-baseline-workbook.py
+++ notebooks/generators/workbooks/02-baseline-workbook.py
@@ -1,23 +1,20 @@
 #%% [markdown]
 # ## Simulated Baseline Generation
-# The simulated baseline curve for this example will be constructed using a simple polynomial curve that will
-# be randomly adjusted between a minimum and maximum exponent value.  Two other baselines will also be used
-# by adding or subtracting in a baseline Gaussian curve.  This baseline Gaussian was selected to overlap the signal
-# region to make the generated data more complex.
+# The simulated baseline curve will be constructed using a simple polynomial curve that will
+# be randomly adjusted between a minimum and maximum exponent value.  The varying exponent in the baseline
+# curve is used to simulate samples having different baselines independent of the concentration level.
 
 #%%
-import os
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-import pandas as pd 
 from scipy.stats import norm    
 
 mpl.style.use('seaborn-notebook')
 plt.rcParams["figure.figsize"] = (12, 5)
 
 #%% [markdown]
-# The following plot shows the minimum and maximum baseline curves.
+# The following plot shows the baseline curves using both the minimum and maximum exponent values.
 
 #%%
 xnum = 600
@@ -28,7 +25,6 @@ E1F_ = E1_ + np.min(E1_)*-1.0
 E2_ = (-1e-7*x**2.2)
 E2F_ = E2_ + np.min(E2_)*-1.0
 
-
 fig, axs = plt.subplots()
 axs.plot(x, E1F_, label='min')
 axs.plot(x, E2F_, label='max')
@@ -36,30 +32,15 @@ fig.suptitle('Baseline Curve Base Shapes')
 plt.legend()
 
 #%% [markdown]
-# The following plot shows the signal Gaussian curves along with the Baseline Gaussian curve.  The
-# baseline Gaussian curve will be used simulate overlapping features and make a more complex curve. 
+# ## Baseline Curves for Dataset Generation
+# This project will vary the baseline curve exponent to generate the simulated datasets.  The goal 
+# of this project is to determine how well each algorithm can handle signals with varying baselines
+# and concentration levels. The following plots show the pure signal combined with each of the minimum
+# and maximum baseline curves at 25% and 75% concentration level.
 
 #%%
 S_1 = norm.pdf(x, loc=310.0, scale=40.0)
 S_2 = norm.pdf(x, loc=390.0, scale=20.0)
-S_3 = norm.pdf(x, loc=360.0, scale=120.0)
-
-fig, axs = plt.subplots()
-axs.plot(x, S_1, label='signal-1')
-axs.plot(x, S_2, label='signal-2')
-axs.plot(x, S_3, label='baseline')
-fig.suptitle('Signal Base Gaussian Shapes with Baseline Gaussian')
-plt.legend()
-
-#%% [markdown]
-# ## Baseline Curves for Dataset Generation
-# This project will use three different algorithms for baseline curves that will be used to generate 
-# the simulated datasets.  The goal of this project is to determine how well each algorithm can handle 
-# these different signals with varying baselines.
-# The following plots show the pure signal combined with each of the baseline curves at 25% and 75% concentration
-# level.
-
-#%%
 S_true = np.vstack((S_1, S_2))
 C_true = np.array([[0.25, 0.75], [0.75, 0.25]])
 signal = np.dot(C_true, S_true)
@@ -69,23 +50,5 @@ for i, level in enumerate(C_true):
     axs.plot(x, signal[i], label='{0:.2f}-signal'.format(C_true[i, 0]))       
     axs.plot(x, signal[i]+E1F_, label='{0:.2f}-min'.format(C_true[i, 0]))
     axs.plot(x, signal[i]+E2F_, label='{0:.2f}-max'.format(C_true[i, 0]))  
-fig.suptitle('Baseline #1 Range (No Baseline Gaussian)')      
-plt.legend()
-
-#%%
-fig, axs = plt.subplots()
-for i, level in enumerate(C_true):
-    axs.plot(x, signal[i], label='{0:.2f}-signal'.format(C_true[i, 0]))       
-    axs.plot(x, signal[i]+E1F_+S_3, label='{0:.2f}-min-pos'.format(C_true[i, 0]))
-    axs.plot(x, signal[i]+E2F_+S_3, label='{0:.2f}-max-pos'.format(C_true[i, 0]))  
-fig.suptitle('Baseline #2 Range (with Pos. Baseline Gaussian)')      
-plt.legend()
-
-#%%
-fig, axs = plt.subplots()
-for i, level in enumerate(C_true):
-    axs.plot(x, signal[i], label='{0:.2f}-signal'.format(C_true[i, 0]))       
-    axs.plot(x, signal[i]+E1F_-S_3, label='{0:.2f}-min-neg'.format(C_true[i, 0]))
-    axs.plot(x, signal[i]+E2F_-S_3, label='{0:.2f}-max-neg'.format(C_true[i, 0]))  
-fig.suptitle('Baseline #3 Range (with Neg. Baseline Gaussian)')      
+fig.suptitle('Baseline Range')      
 plt.legend()
