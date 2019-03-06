8cd295291ad4994812f6547e06963c96eb9b5504
Michael Siers <msiers@saturnsys.com>
2019-03-06 09:44:18 -0600

    Updated the signal generation notebook.

--- notebooks/generators/workbooks/01-signal-workbook.py
+++ notebooks/generators/workbooks/01-signal-workbook.py
@@ -6,11 +6,9 @@
 # the concentration value. 
 
 #%%
-import os
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-import pandas as pd 
 from scipy.stats import norm    
 
 mpl.style.use('seaborn-notebook')
@@ -35,7 +33,9 @@ axs.plot(x, S_2)
 fig.suptitle('Signal Base Gaussian Shapes')
 
 #%% [markdown]
-# The following plot shows the combined Gaussian peaks with varying concentration levels.
+# The following plots will show the combined Gaussian peaks at varying concentration levels.  The first
+# plot is generated using a fixed set of concentrations values.  The second plot generates a random set of
+# curves with the line color determined from the concentration level.
 
 #%%
 C_true = np.array([[0.0, 1.0], [0.25, 0.75], [0.5, 0.5], [0.75, 0.25], [1.0, 0.0]])
@@ -47,3 +47,34 @@ for i, level in enumerate(C_true):
 fig.suptitle('Combined Signal at Varying Concentration Levels')
 plt.legend()
 
+#%%
+nsamples = 100
+C_levels = np.random.rand(nsamples)
+C_matrix = np.vstack((C_levels, (1-C_levels))).T
+samples = np.dot(C_matrix, S_true)
+
+# norm is a class which, when called, can normalize data into the
+# [0.0, 1.0] interval.
+norm = mpl.colors.Normalize(vmin=np.min(C_levels), vmax=np.max(C_levels))
+
+# create a ScalarMappable and initialize a data structure
+jet = plt.get_cmap('jet')
+s_m = mpl.cm.ScalarMappable(norm=norm, cmap=jet)
+s_m.set_array([])
+
+# plotting spectra
+# calling the ScalarMappable that was initialised with c_m and norm
+fig, axs = plt.subplots()
+for i in range(nsamples):
+    color = s_m.to_rgba(C_levels[i])
+    axs.plot(x, samples[i, :].T, color=color)
+
+# we plot the colorbar, using again our
+# ScalarMappable
+c_bar = plt.colorbar(s_m)
+c_bar.set_label(r"C_")
+
+fig.suptitle('Combined Signals Colored By Concentration Levels')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
