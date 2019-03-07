d912315fbdfd41a63cb2db3dd7b13be6c3dec23b
Michael Siers <msiers@saturnsys.com>
2019-03-07 08:45:49 -0600

    Updated the signal preprocessing notebook.

--- notebooks/processing/workbooks/01-signal-preprocessing.py
+++ notebooks/processing/workbooks/01-signal-preprocessing.py
@@ -1,3 +1,9 @@
+#%% [markdown]
+# ## Signal Preprocessing
+# Before the raw signal can be used by the machine learning algorithms it must be preprocessed.  This
+# notebook will perform the different preprocessing steps on some sample signals to visually verify they
+# are functioning properly.
+
 #%%
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -13,6 +19,13 @@ from src.models.transformers.truncate import Truncate
 mpl.style.use('seaborn-notebook')
 plt.rcParams["figure.figsize"] = (12, 5)
 
+#%% [markdown]
+# ## Encoding Target Values
+# The target value for this project is a floating point concentation value.  This value must be
+# encoded into a class value in order to help analyze the results of each algorithm.  The following
+# code will geneate a sample with one in each of the target classes.  The encoders are then used
+# determine the class values.
+
 #%%
 cvals = np.array([0.15, 0.50, 0.85])
 xvalues, ylevel, blexps, ydata0 = data_generator_levels(cvals)
@@ -23,6 +36,20 @@ ymulti = LevelMulti(targetmin=0.2, targetmax=0.8).transform(cvals)
 print(cvals)
 print(ynormal)
 print(ymulti)
+
+#%% [markdown]
+# ## Preprocessing Pipeline
+# The raw signal from each sample will be run through the different steps of the preprocessing
+# pipeline.  The following code and plots will show the output of these different steps to ensure 
+# the entire transformation produces the best input signal for the machine learning 
+# algorithms.  The following table gives a brief description of each preprocessing step.
+#
+# | Step      | Description                                                                          |
+# |:----------|:-------------------------------------------------------------------------------------|
+# | filter    | Applys Savitsky-Golay filter to raw data to smooth out the signal.                   |
+# | baseline  | Determines baseline signal from the input signal.                                    |
+# | correct   | Performs baseline correction by subtracting off the baseline signal.                 |
+# | truncate  | Slices the input signal to only output the region of interest from the signal.       |
 
 #%%
 xmin = 200
@@ -39,6 +66,11 @@ ydata_bl = datapipeline.named_steps['baseline'].transform(ydata_fl.copy())
 ydata_cs = datapipeline.named_steps['correct'].transform(ydata_fl.copy())
 ydata_tr = datapipeline.named_steps['truncate'].transform(ydata_cs.copy())
 
+#%% [markdown]
+# ## Baseline Signal (Full Signal)
+# The following set of plots will show the raw signal and the computed baseline signal for
+# each sample.  The pure signal used to generate the sample is also displayed. 
+
 #%%
 for i in range(3):
     fig, axs = plt.subplots()
@@ -47,6 +79,12 @@ for i in range(3):
     axs.plot(xvalues, ydata_bl[i], label='baseline')    
     fig.suptitle('Sample:[{0}] Baseline:[{1:.4f}] Target:[{2:.4f}]'.format(i, blexps[i], ylevel[i]))         
     plt.legend()
+
+#%% [markdown]
+# ## Baseline Signal (Region of Interest)
+# The following set of plots will show the different lines computed from the preprocessing 
+# pipeline in only the region of interest for this analysis.  It will include the filtered/smoothed
+# signal line since this line is visible is this chart.
 
 #%%
 for i in range(3):
@@ -58,11 +96,16 @@ for i in range(3):
     fig.suptitle('Sample:[{0}] Baseline:[{1:.4f}] Target:[{2:.4f}]'.format(i, blexps[i], ylevel[i]))         
     plt.legend()
 
+#%% [markdown]
+# ## Baseline Corrected Signal (Region of Interest)
+# The following set of plots will show the baseline corrected lines computed from the preprocessing 
+# pipeline in only the region of interest for this analysis.  This data displayed in these charts
+# will end up being the data that will be used by the machine learning algorithms.
+
 #%%
 for i in range(3):
     fig, axs = plt.subplots()
     axs.plot(xvalues[xmin:xmax+1], signals[i, xmin:xmax+1], label='signal')    
-    #axs.plot(xvalues[xmin:xmax+1], ydata_cs[i, xmin:xmax+1], label='corrected')
     axs.plot(xvalues[xmin:xmax+1], ydata_tr[i], label='corrected')
     fig.suptitle('Sample:[{0}] Baseline:[{1:.4f}] Target:[{2:.4f}]'.format(i, blexps[i], ylevel[i]))         
     plt.legend()
