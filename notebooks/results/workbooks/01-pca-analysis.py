f4fc0bc7307faa9a0ce311d52dbf63a87be9ab8b
Michael Siers <msiers@saturnsys.com>
2019-03-11 12:43:50 -0500

    Finished documenting the analysis notebooks.

--- notebooks/results/workbooks/01-pca-analysis.py
+++ notebooks/results/workbooks/01-pca-analysis.py
@@ -1,27 +1,27 @@
 #%% [markdown]
-# ## PCA Analysis Results
-# The goal of this notebook is to document the PCA results on the simulated datasets.
+# ## PCA Dataset Analysis
+# The goal of this notebook is to review the PCA algorithm outputs on the simulated datasets.  The
+# [**Silhoutte Score**](https://scikit-learn.org/stable/modules/generated/sklearn.metrics.silhouette_score.html) 
+# metric value will be computed for each of the datasets.  This value is a metric that trys to determine 
+# how well the different samples cluster together.  The best value is 1.0 and the worst value is -1.0.  It will 
+# be used to compare the datasets against each other.
 
 #%%
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from IPython.display import set_matplotlib_formats
 import numpy as np
 import pandas as pd
 from sklearn import metrics
 from src.models.encoders.levelmulti import LevelMulti
 from src.models.pipelines.pipelines import pca_pipeline
 
-%matplotlib inline
-set_matplotlib_formats('png', 'pdf')
 mpl.style.use('seaborn-notebook')
 plt.rcParams["figure.figsize"] = (8, 6)
 
 #%%
 def load_data(filename):
     df = pd.read_csv(filename, index_col=0)
-    ylevel = df['level'].values.copy()  
-    #blexps = dfFile['blexp'].values.copy()       
+    ylevel = df['level'].values.copy()    
     dfData = df.drop(['level', 'blexp'], axis=1).copy()
     return ylevel, dfData.values.copy()
 
@@ -56,3 +56,9 @@ pca_plot(2, 'filtered')
 
 #%%
 pca_plot(2, 'baseline')
+
+#%% [markdown]
+# ## PCA Dataset Results
+# Using the PCA algorithm, the **Silhoutte Score** is signficantly higher for the dataset that includes 
+# the baseline correction step.  This is an expected result since the goal of baseline correction is to 
+# remove the variance from the data that is not related to the actual target signal.
